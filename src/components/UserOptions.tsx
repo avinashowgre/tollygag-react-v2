85e711edf20eb7e288eb7564e1fde273e1e11dab
avinashow <avinashow@users.noreply.github.com>
2020-10-06 19:59:08 -0700

    Signup page fix

--- src/components/UserOptions.tsx
+++ src/components/UserOptions.tsx
@@ -3,11 +3,11 @@ import { makeStyles } from "@material-ui/core/styles";
 
 import AccountCircle from "@material-ui/icons/AccountCircle";
 
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import { Login } from "./Login";
+import { SignUp } from "./SignUp";
 
 type UserOptionsProps = any;
 
@@ -59,9 +59,7 @@ export function UserOptions(props: UserOptionsProps) {
       ) : (
         <Fragment>
           <Login />
-          <Button color="primary" variant="contained" className={classes.btn}>
-            Register
-          </Button>
+          <SignUp />
         </Fragment>
       )}
     </div>
